f8a8ec0087ac0599e3162daa089c5197f28cbfe3
jesinjaice <j99615d@gmail.com>
2018-03-14 21:20:24 +0530

    initial commit

--- app/src/main/java/myapp/jesin/com/blood4life/Emergency.java
+++ app/src/main/java/myapp/jesin/com/blood4life/Emergency.java
@@ -22,37 +22,13 @@ import java.util.HashMap;
 import java.util.List;
 
 
-public class Emergency extends ListActivity {
+public class Emergency extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
 
-        Parse.initialize(this);
-        ParseQuery<ParseObject> query=new ParseQuery("Emergency");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-                if (e==null){
-                    ArrayList<HashMap<String,String>> articles=new ArrayList<HashMap<String, String>>();
-                    for (ParseObject object:objects){
-                        HashMap<String,String>article=new HashMap<String, String>();
-                        article.put("bloodtype",object.getString("bloodtype"));
-                        article.put("unitblood",object.getString("unitblood"));
-                        article.put("city",object.getString("city"));
-                        article.put("district",object.getString("district"));
-                        article.put("nameconatact",object.getString("nameconatact"));
-                        article.put("numcontact",object.getString("numcontact"));
-                        articles.add(article);
-                    }
-                    SimpleAdapter adapter=new SimpleAdapter(Emergency.this,articles,android.R.layout.simple_list_item_2,new String[]{"bloodtype","unitblood","city","district","nameconatact,numcontact"},new int[]{R.id.tbld,R.id.tunit,R.id.tcity,R.id.tdist,R.id.tname,R.id.tnum});
-                    setListAdapter(adapter);
-                }
-
-
-            }
-        });
 
 
     }
